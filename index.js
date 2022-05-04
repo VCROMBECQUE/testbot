82647e1f463faff25a704a39fa655c48e99198ee
Valentin <valentin.crombecque@gmail.com>
2022-05-05 01:17:28 +0200

    wip

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-require('dotenv').config({path: __dirname + '/.env'})
+require('dotenv').config()
 
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
 
 const token = process.env['BOT_TOKEN']
 
+
 client.once('ready', () => {
    console.log('Félicitations, votre bot Discord a été correctement initialisé !');
+
+   client.on("message", message => {
+      if (message.content === "!ping") {
+        message.channel.send("Vive les poules !")
+      }
+    })
+
 });
 
 client.login(token);
 
-// bot.on('message', message => {
-//     if (message.content === 'ping') {
-//       message.reply('pong !')
-//     }
-//   })
