7169fdb8248933ffe61c873283a7155cbec4f732
Valentin <valentin.crombecque@gmail.com>
2022-05-05 01:06:54 +0200

    wip

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+require('dotenv').config({path: __dirname + '/.env'})
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const token = "OTcxNTM2OTE3MjczMzg3MDA4.YnL8Qw.1JqvXXSXvEr-ZasblKfw5GQ2aFk"
+
+const token = process.env['BOT_TOKEN']
 
 client.once('ready', () => {
    console.log('Félicitations, votre bot Discord a été correctement initialisé !');
